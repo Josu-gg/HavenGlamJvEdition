ac34d725e13ca29f783b34c479e3a3c3fac9543c
Josue-gg <bp25001@esfe.agape.edu.sv>
2026-06-14 17:00:24 -0600

    Se crearon los metodos de Categoria, crud

--- src/main/java/persistencia/CategoriaDAO.java
+++ src/main/java/persistencia/CategoriaDAO.java
@@ -48,6 +48,85 @@ public class CategoriaDAO
         }
         return result;
     }
+    public  boolean update(Categoria categoria) throws SQLException {
+        boolean res = false;
+        try {
+            ps = conn.connect().prepareStatement(
+                    "Update Categoria " +
+                            "Set NombreCategoria = ?, IdCategoria = ?" +
+                            "Where IdCategoria = ? ");
+            ps.setString(1, categoria.getNombreCategoria());
+            ps.setInt(2, categoria.getIdEstado());
+            ps.setInt(3, categoria.getIdCategoria());
+            if (ps.executeUpdate() > 0) {
+                res = true;
+            }
+            ps.close();
+        }
+        catch (SQLException ex){
+            throw new SQLException("Error al modificar categoria: "
+            + ex.getMessage(), ex);
+        } finally {
+            ps = null;
+            conn.disconnect();
+        }
+        return res;
+
+    }
+    public boolean delete(Categoria categoria)throws  SQLException{
+        boolean res = false;
+        try {
+            ps = conn.connect().prepareStatement(
+                    "Delete From Categoria Where IdCategoria = ?"
+            );
+            ps.setInt(1, categoria.getIdCategoria());
+            if (ps.executeUpdate() > 0) {
+                res = true;
+            }
+            ps.close();
+        }catch (SQLException ex){
+            throw  new SQLException("Error al eliminar una categoria: "+
+                    ex.getMessage(), ex);
+
+        }finally {
+            ps = null;
+            conn.disconnect();
+        }
+
+        return  res;
+    }
+    public Categoria getById(int pId) throws  SQLException {
+        Categoria categoria = new Categoria();
+        try {
+            ps = conn.connect().prepareStatement(
+                    "Select IdCategoria, NombreCategoria, IdEstado " +
+                            "From Categoria " +
+                            "Where IdCategoria = ?"
+            );
+            ps.setInt(1, pId);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                categoria.setIdCategoria(rs.getInt(1));
+                categoria.setNombreCategoria(rs.getString(2));
+                categoria.setIdEstado(rs.getInt(3));
+            }
+            else {
+                categoria = null;
+            }
+            ps.close();
+            rs.close();
+        }
+        catch (SQLException ex){
+            throw  new SQLException("Error al obtener una categoria por id: "
+            + ex.getMessage(), ex);
+        } finally {
+            ps = null;
+            rs = null;
+            conn.disconnect();
+        }
+        return categoria;
+    }
 }
 
 
